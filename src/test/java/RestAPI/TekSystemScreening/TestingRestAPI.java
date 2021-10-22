efb15f349c086cc3a518ff0451845388a442c5bc
Priten Patel <pritenpatel1@gmail.com>
2021-10-22 15:10:08 -0400

    Updated

--- src/test/java/RestAPI/TekSystemScreening/TestingRestAPI.java
+++ src/test/java/RestAPI/TekSystemScreening/TestingRestAPI.java
@@ -28,31 +28,31 @@ import java.util.Scanner;
 
 public class TestingRestAPI {
 
-	public String responseInString;
+	public static String responseInString;
 	public String countryNameOrCode;
 	public int inputCharacterCount;
 	public String basePathForCountryName;
 	public String basePathForCountryCode;
 	RequestSpecification request;
 	static int statusCode;
-	TestingRestAPI tpiObject;
+	static TestingRestAPI tpiObject;
 	static String retry;
 	static String countryName;
 	static String capitalName;
 
 	
 	
-	@Test(priority = 2)
+	
 	public void getInputFromUser() {
 		System.out.println("Please Enter the Country Name Or Country Code whose Capital you Wish to check");
 		System.out.println("For Example: United States or US");
 		Scanner sc = new Scanner(System.in);
 		countryNameOrCode = sc.nextLine();
-		System.out.print("You requested to get the capital for: " + countryNameOrCode + "\n");
+		System.out.print("You requested to get the capital for: " + countryNameOrCode + "\n \n");
 
 	}
 
-	@Test(priority = 3)
+	
 	public void getRequest() throws JsonMappingException, JsonProcessingException {
 		RestAssured.baseURI = "https://restcountries.com/v3.1/";
 
@@ -75,13 +75,13 @@ public class TestingRestAPI {
 
 		responseInString = response.asString();
 		JsonPath js = new JsonPath(responseInString);
+		
 
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		// Get tree representation of json
 		JsonNode jsonTree = objectMapper.readTree(responseInString);
 		statusCode = response.getStatusCode();
-		System.out.println("before extracting size");
 		String xx=response.asString();
 		String connectionHeader=response.header("Connection");
 		String keepAliveHeader=response.header("Keep-Alive");
@@ -90,7 +90,7 @@ public class TestingRestAPI {
 		Assert.assertEquals("timeout=5, max=100",keepAliveHeader);
 		int size=xx.length();
 		if(size==2) {
-			System.out.println("The Country code/Name you have entered is invalid, Please enter a valid country");
+			System.out.println("The Country code/Name you have entered is invalid, Please enter a valid country\n");
 			retry="Yes";
 		}
 		else if (statusCode == 200 && size>2) {
@@ -106,19 +106,20 @@ public class TestingRestAPI {
 			System.out.println("POSITIVE TESTCASE PASS- Keep-Alive Header Value as Expected");
 			Assert.assertEquals(200, statusCode);
 			System.out.println("POSITIVE TESTCASE PASS- Status Code 200 as Expected \n");
-			System.out.println("**************************NOTE END**************************************");
+			System.out.println("The Response From The Service"+responseInString);
+			System.out.println("**************************NOTE END************************************** \n \n");
 		}
 
 	}
 
 	
-	@Test(priority = 4)
+	
 	public void lastMethod() throws JsonMappingException, JsonProcessingException {
         
 		if (statusCode == 404 || statusCode == 400) {
 			System.out.println("Uh Ohh!!! There is No Such Country, Please try again with Valid Country \n \n");
 			System.out.println(
-					"If you would like to try again? Please Enter Yes to Continue " + "Or Type Exit to Quit \n");
+					"If you would like to try again? Please Enter Yes to Continue " + "Or Type Exit to see the Test Execution Results \n");
 			Scanner sc = new Scanner(System.in);
 			retry = sc.nextLine();
 			while (!retry.equalsIgnoreCase("Yes") && !retry.equalsIgnoreCase("exit")) {
@@ -130,7 +131,7 @@ public class TestingRestAPI {
 		}
 	}
 
-	@Test(priority = 5)
+	
 	public void reRun() throws JsonMappingException, JsonProcessingException {
 		tpiObject = new TestingRestAPI();
 		if (statusCode == 200 && countryName != null) {
@@ -139,7 +140,7 @@ public class TestingRestAPI {
 			}
 			System.out.println("\nGreat Awesome!!! You Found the Capital of " + countryName + ", "
 					+ "\nNow Would you like to find Capitals of Other Countries?"
-					+ "\nIf yes, Please type YES to Continue Or Type EXIT to Quit");
+					+ "\nIf yes, Please type YES to Continue Or Type EXIT to see the Test Execution Results");
 			Scanner sc = new Scanner(System.in);
 			retry = sc.nextLine();
 
@@ -157,9 +158,9 @@ public class TestingRestAPI {
 				if (countryName.equalsIgnoreCase(null)) {
 					System.out.println("Please Enter a Valid Country code which matches to a Country");
 				}
-				System.out.println("Awesome!!! You Found the Capital of" + countryName + ", "
+				System.out.println("Awesome!!! You Found the Capital of " + countryName + ", "
 						+ "\nNow Would you like to find Capitals of Other Countries?"
-						+ "\nIf yes, Please type YES to Continue Or Type EXIT to Quit");
+						+ "\nIf yes, Please type YES to Continue Or Type EXIT to see the Test Execution Results");
 				Scanner sc = new Scanner(System.in);
 				retry = sc.nextLine();
 
@@ -175,7 +176,7 @@ public class TestingRestAPI {
 
 	}
 	
-	@Test(priority = 6)
+	
 	public void negativeTestCase01() throws JsonMappingException, JsonProcessingException {
 		RestAssured.baseURI = "https://restcountries.com/v3.1/";
        
@@ -191,7 +192,7 @@ public class TestingRestAPI {
 				responseInString = response.asString();
 				JsonNode jsonTree = objectMapper.readTree(responseInString);
 				statusCode = response.getStatusCode();
-				Assert.assertEquals(404, statusCode);
+				Assert.assertEquals(40, statusCode);
 				System.out.println("\n**************************NOTE BEGIN*******************************************************");
 				 System.out.println("\nFirst Negative Test case is being executed: Using Incorrect Country Name");
 				System.out.println("Negative Test Case: PASS : Error code 404 as Expected");
@@ -204,7 +205,7 @@ public class TestingRestAPI {
 		
 	}
 	
-	@Test(priority = 7)
+	
 	public void negativeTestCase02() throws JsonMappingException, JsonProcessingException {
 		RestAssured.baseURI = "https://restcountries.com/v3.1/";
         
@@ -227,9 +228,61 @@ public class TestingRestAPI {
 				String messageValue=jsonTree.at("/message").asText();
 				Assert.assertEquals("Not Found", messageValue);
 				System.out.println("Negative Test Case: PASS : Message Value in Response ="+messageValue+" As Expected\n");
+				System.out.println("The Response From The Service"+responseInString);
 				System.out.println("**************************NOTE END********************************************************\n");
 				
 				
+		
+	}
+	
+	public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
+		
+		
+		tpiObject = new TestingRestAPI();
+		tpiObject.getInputFromUser();
+		tpiObject.getRequest();
+		tpiObject.lastMethod();
+		if (statusCode == 200 && countryName != null) {
+			if (countryName == null) {
+				System.out.println("Please Enter a Valid Country code which matches to a Country");
+			}
+			System.out.println("\nGreat Awesome!!! You Found the Capital of " + countryName + ", "
+					+ "\nNow Would you like to find Capitals of Other Countries?"
+					+ "\nIf yes, Please type YES to Continue Or Type EXIT to see the Test Execution Results");
+			Scanner sc = new Scanner(System.in);
+			retry = sc.nextLine();
+
+			while (!retry.equalsIgnoreCase("Yes") && !retry.equalsIgnoreCase("exit")) {
+				System.out.println("INVALID RESPONSE!!!- Please Type YES or EXIT");
+				retry = sc.nextLine();
+			}
+
+		}
+		while (!retry.equalsIgnoreCase("Exit")) {
+			tpiObject.getInputFromUser();
+			tpiObject.getRequest();
+			tpiObject.lastMethod();
+			if (statusCode == 200 && countryName != null) {
+				if (countryName.equalsIgnoreCase(null)) {
+					System.out.println("Please Enter a Valid Country code which matches to a Country");
+				}
+				System.out.println("Awesome!!! You Found the Capital of" + countryName + ", "
+						+ "\nNow Would you like to find Capitals of Other Countries?"
+						+ "\nIf yes, Please type YES to Continue Or Type EXIT to see the Test Execution Results");
+				Scanner sc = new Scanner(System.in);
+				retry = sc.nextLine();
+
+				while (!retry.equalsIgnoreCase("Yes") && !retry.equalsIgnoreCase("exit")) {
+					System.out.println("INVALID RESPONSE!!!- Please Type YES or EXIT");
+					retry = sc.nextLine();
+				}
+
+			}
+		}
+		tpiObject.negativeTestCase01();
+		tpiObject.negativeTestCase02();
+		
+		
 		
 	}
 	
