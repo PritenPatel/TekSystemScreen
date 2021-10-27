3be5eb38c444090eb26bba51730ab512aaf716c0
Priten Patel <pritenpatel1@gmail.com>
2021-10-27 19:57:29 -0400

    Commit to Tuesday Branch

---
+++ TekSystemScreening/src/test/java/RestAPI/TekSystemScreening/Eng.java
@@ -0,0 +1,22 @@
+package RestAPI.TekSystemScreening;
+
+public class Eng {
+
+	String official;
+	String common;
+	
+	
+	public String getOfficial() {
+		return official;
+	}
+	public void setOfficial(String official) {
+		this.official = official;
+	}
+	public String getCommon() {
+		return common;
+	}
+	public void setCommon(String common) {
+		this.common = common;
+	}
+	
+}
---
+++ src/test/java/RestAPI/TekSystemScreening/TestingPojos.java
@@ -0,0 +1,15 @@
+package RestAPI.TekSystemScreening;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestingPojos {
+	
+	public static void main(String[] args) {
+		
+		Eng engObj=new Eng();
+		ObjectMapper objMapper = new ObjectMapper();
+		
+		
+	}
+
+}
